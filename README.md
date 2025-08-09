22eeb1d90860dc47af5b5c1bf628704d3a771a87
𝓑𝓞𝓟👑 <matthieu.ballout@isen-ouest.yncrea.fr>
2025-08-09 23:27:56 +0200

    add file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Learning-C-Mini-project-
 This repo is a collection of mini projects in C++ that I created to learn to master the basics of the language
+
+## Calculator
+ex: `.\calculator.exe --add 2 2 --sous 2 3 --mult 5 5 --div 9 3`
+result: `4 1 25 3`
---
+++ calculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+int addition(int a, int b) { return a + b; }
+int soustraction(int a, int b) { return abs(a - b); }
+int multiplication(int a, int b) { return a * b; }
+int division(int a, int b) { return (a != 0 && b != 0) ? a / b : 1; }
+
+int main(int argc, char *argv[]) {
+    int nbr1 = 0, nbr2 = 0;
+    for (int i = 0; i < argc; i++) {
+        if (std::string(argv[i]) == "--add") {
+            nbr1 = atoi(argv[i+1]), nbr2 = atoi(argv[i+2]);
+            std::cout << addition(nbr1, nbr2) << std::endl;
+        } else if (std::string(argv[i]) == "--sous") {
+            nbr1 = atoi(argv[i+1]), nbr2 = atoi(argv[i+2]);
+            std::cout << soustraction(nbr1, nbr2) << std::endl;
+        } else if (std::string(argv[i]) == "--mult") {
+            nbr1 = atoi(argv[i+1]), nbr2 = atoi(argv[i+2]);
+            std::cout << multiplication(nbr1, nbr2) << std::endl;
+        } else if (std::string(argv[i]) == "--div") {
+            nbr1 = atoi(argv[i+1]), nbr2 = atoi(argv[i+2]);
+            std::cout << division(nbr1, nbr2) << std::endl;
+        }
+    }
+    return 0;
+}
